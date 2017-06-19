ea2c9965cf22c86a9da655ff6ffe887fe7e3ebf7
flubox <francois.lunette@photobox.com>
2017-06-19 18:11:10 +0200

    camera position update

--- src/VirtualShowRoom.js
+++ src/VirtualShowRoom.js
@@ -7,7 +7,7 @@ export const VirtualShowRoom = props => {
                 <a-asset-item id="room-obj" src="./src/room.obj"></a-asset-item>
                 <a-asset-item id="room-mtl" src="./src/room.mtl"></a-asset-item>
             </a-assets>
-            <a-camera id="main-camera"/>
+            <a-camera id="main-camera" position="0.09 0 -2.19" rotation="-40.33 172.575"/>
             <a-entity obj-model="obj: #room-obj; mtl: #room-mtl"></a-entity>
         </a-scene>
     );
