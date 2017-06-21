044904b290d2a4e4c2444d7bbcd253d049fb42ec
cedricburonphotobox <cedric.buron@photobox.com>
2017-06-21 14:03:41 +0200

    APP : new model

--- src/VirtualShowRoom.js
+++ src/VirtualShowRoom.js
@@ -4,8 +4,8 @@ export const VirtualShowRoom = props => {
     return (
         <a-scene>
             <a-assets>
-                <a-asset-item id="room-obj" src="./src/room.obj"></a-asset-item>
-                <a-asset-item id="room-mtl" src="./src/room.mtl"></a-asset-item>
+                <a-asset-item id="room-obj" src="./src/room_1.obj"></a-asset-item>
+                <a-asset-item id="room-mtl" src="./src/room_1.mtl"></a-asset-item>
             </a-assets>
             <a-camera id="main-camera" position="0.09 0 -2.19" rotation="-40.33 172.575"/>
             <a-entity obj-model="obj: #room-obj; mtl: #room-mtl"></a-entity>
